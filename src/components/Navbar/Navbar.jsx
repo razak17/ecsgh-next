fd8036bf3bac9d1efe75aecd611775d49547422b
Razak <moabdulrazak17@gmail.com>
2022-06-26 21:57:32 +0000

    Fix: Fully functional Nav Menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
 import style from './Navbar.module.css';
-import { Link } from 'react-scroll';
 import { menuItems } from '../Header/Header.data';
 import Logo from '../Partials/Logo';
 import LinkItem from 'components/Partials/LinkItem';
@@ -13,29 +14,37 @@ const Navbar = () => {
 			<Logo to='intro' label='ECSGH' />
 			<ul className={style.list}>
 				{menuItems.map(({ path, label }, index) => (
-					<li className={style.listItem} key={index}>
+					<li className={style.list_item} key={index}>
 						<LinkItem to={path} label={label} key={index} />
 					</li>
 				))}
 			</ul>
-
-			{/* Hamburger Menu */}
-			<div className={style.hamburger} onClick={() => setOpen(!open)}>
-				<div className={style.line} />
-				<div className={style.line} />
-				<div className={style.line} />
-			</div>
-			<ul
-				onClick={() => setOpen(false)}
-				className={style.menu}
-				style={{ right: open ? '0px' : '-50vw' }}
-			>
-				{menuItems.map(({ path, label }, index) => (
-					<li className={style.menuItem} key={index}>
-						<LinkItem to={path} label={label} key={index} />
-					</li>
-				))}
-			</ul>
+					<button
+						className={`${style.nav_btn} ${style.nav_bars_btn}`}
+						onClick={() => setOpen(!open)}
+					>
+				<FaBars />
+			</button>
+			{open && (
+				<div className={style.nav}>
+					<ul onClick={() => setOpen(false)} className={style.menu}>
+						{menuItems.map(({ path, label }, index) => (
+							<li className={style.menu_item} key={index}>
+								{/* <LinkItem to={path} label={label} key={index} /> */}
+								<a href={`#${path}`} onClick={() => setOpen(!open)}>
+									{label}
+								</a>
+							</li>
+						))}
+					</ul>
+					<button
+						className={`${style.nav_btn} ${style.nav_close_btn}`}
+						onClick={() => setOpen(!open)}
+					>
+						<FaTimes />
+					</button>
+				</div>
+			)}
 		</div>
 	);
 };
