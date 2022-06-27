6f8df258fdb0a658a09e193790a792a4148a986e
Razak <moabdulrazak17@gmail.com>
2022-06-27 21:24:50 +0000

    Feat: Implement sending email using emailJS

--- src/components/Layout/Contact/Contact.jsx
+++ src/components/Layout/Contact/Contact.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import emailjs from '@emailjs/browser';
+
 import Title from '../../Partials/Title';
 import SubTitle from '../../Partials/SubTitle';
 import AppWrap from '../../Wrapper/AppWrap';
@@ -5,37 +8,116 @@ import AppWrap from '../../Wrapper/AppWrap';
 import style from './Contact.module.css';
 
 const Contact = () => {
+	const [ formData, setFormData ] = useState({
+		first_name: '',
+		last_name: '',
+		email: '',
+		subject: '',
+		message: '',
+	});
+	const [ loading, setLoading ] = useState(false);
+
+	const handleChangeInput = e => {
+		const { name, value } = e.target;
+		setFormData({ ...formData, [ name ]: value });
+	};
+
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		// console.log(formData);
+		const { first_name, last_name, email, subject, message } = formData;
+
+		emailjs
+			.send(
+				'service_ecsgh',
+				'template_ecsgh',
+				{
+					first_name,
+					last_name,
+					email,
+					subject,
+					message,
+				},
+				'HmrC75Jr4au543Xni'
+			)
+			.then(
+				res => {
+					console.log(res.text);
+					setFormData({
+						first_name: '',
+						last_name: '',
+						email: '',
+						subject: '',
+						message: '',
+					});
+				},
+				error => {
+					console.log('Email JS Error', error.text);
+				}
+			);
+	};
+
 	return (
 		<>
 			<Title title='Get In Touch' />
 			<SubTitle subTitle='Contact Us' />
 			<div className={style.contact}>
 				<div className={style.contact_form}>
-					<form>
+					<form onSubmit={handleSubmit}>
 						<div className={style.row}>
 							<div className={style.input50}>
-								<input type='text' placeholder='First Name' />
+								<input
+									type='text'
+									name='first_name'
+									placeholder='First Name'
+									value={formData.first_name}
+									onChange={handleChangeInput}
+								/>
 							</div>
 							<div className={style.input50}>
-								<input type='text' placeholder='Last Name' />
+								<input
+									type='text'
+									name='last_name'
+									placeholder='Last Name'
+									value={formData.last_name}
+									onChange={handleChangeInput}
+								/>
 							</div>
 						</div>
 						<div className={style.row}>
 							<div className={style.input50}>
-								<input type='text' placeholder='Email' />
+								<input
+									type='text'
+									name='email'
+									placeholder='Email'
+									value={formData.email}
+									onChange={handleChangeInput}
+								/>
 							</div>
 							<div className={style.input50}>
-								<input type='text' placeholder='Subject' />
+								<input
+									type='text'
+									name='subject'
+									placeholder='Subject'
+									value={formData.subject}
+									onChange={handleChangeInput}
+								/>
 							</div>
 						</div>
 						<div className={style.row}>
 							<div className={style.input100}>
-								<textarea placeholder='Message'></textarea>
+								<textarea
+									name='message'
+									placeholder='Message'
+									value={formData.message}
+									onChange={handleChangeInput}
+								></textarea>
 							</div>
 						</div>
 						<div className={style.row}>
 							<div className={style.input100}>
-								<input type='submit' value='Send Message' />
+								<input type='submit' value={loading ? 'Sending' : 'Send Message'} />
 							</div>
 						</div>
 					</form>
@@ -57,7 +139,7 @@ const Contact = () => {
 					<div className='style.info-box'>
 						<div className='style.details'>
 							<h4>Phone number</h4>
-              <a href="tel:+19784444444">+1 978 555 4444</a>
+							<a href='tel:+19784444444'>+1 978 555 4444</a>
 						</div>
 					</div>
 				</div>
